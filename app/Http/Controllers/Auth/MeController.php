a2ac2369b69814b5f17742c0ad1a080dafe665ae
mgkyawzayya <kyawzayya.ucsp@gmail.com>
2020-02-24 01:55:32 +0630

    Lesson-15

---
+++ app/Http/Controllers/Auth/MeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Transformers\Users\UserTransformer;
+use Illuminate\Http\Request;
+
+class MeController extends Controller
+{
+    /**
+     * MeController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth:api']);
+    }
+    public function __invoke(Request $request)
+    {
+        return fractal()
+            ->item($request->user())
+            ->transformWith(new UserTransformer())
+            ->toArray();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+    Route::post('signin', 'SignInController');
+    Route::get('me', 'MeController');
+    Route::post('signout', 'SignOutController');
+});
